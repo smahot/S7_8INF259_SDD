f2df07f1f89bdcee5fca01c520e03fc8a7c74e45
smahot <steve.mahot@edu.devinci.fr>
2019-09-12 15:40:42 -0400

    clean

--- S7_8INF259_SDD_TP1/dossierProfesseur.h
+++ S7_8INF259_SDD_TP1/dossierProfesseur.h
@@ -16,3 +16,16 @@ struct Professeur{
 	cours* listeCours;
 	Professeur* suivant;
 };
+
+class DossierProfesseur {
+private:
+	Professeur* tete; // début de la liste chaînée.
+public:
+	DossierProfesseur(char* FP); // construit la liste chaînée en mémoire à partir du fichier FP.
+	~DossierProfesseur(); // détruit la liste chaînée existant en mémoire.
+	void Supprimer(char* NOM); // supprime de la liste chaînée le professeur NOM.
+	int Commun(char* X, char* Y); // affiche le nombre de cours en commun entre les profs X et Y.
+	char* LecoursLeplusDemande() const; // affiche le cours le plus demandé de la liste chaînée. En	cas de solutions multiples, prendre celui ayant le plus d’étudiants.
+	char* ProfeseurLeplusAncien() const; // affiche le nom du professeur le plus ancien.
+	void Recopier(char* Nouveau); // recopie à partir de la liste chaînée le nom, ancienneté et le nombre de cours demandés pour chaque professeur dans le fichier texte Nouveau.
+};
--- S7_8INF259_SDD_TP1/main.cpp
+++ S7_8INF259_SDD_TP1/main.cpp
@@ -6,5 +6,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n"; 
+    cout << "Hello World!\n";
+	system("pause");
 }
