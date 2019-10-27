f37e2eff728ebbecf324237f4ff108928dc2c1c1
smahot <steve.mahot@edu.devinci.fr>
2019-10-27 19:14:19 -0400

    ajout méthode prefix

--- S7_8INF259_SDD_TP2/S7_8INF259_SDD_TP2.cpp
+++ S7_8INF259_SDD_TP2/S7_8INF259_SDD_TP2.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "prefix.h"
+#include <vector>
+#include <windows.h>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//SetConsoleOutputCP(CP_UTF8);
+
+	string expression = "5+(10/2)*3";
+	std::vector<char> vect(expression.begin(), expression.end());
+	Prefix p = Prefix(expression);
+
+	cout << p.prefixe(vect) << endl;
+
+
+
+	std::cout << "fin programme" << endl;
 }
--- S7_8INF259_SDD_TP2/prefix.cpp
+++ S7_8INF259_SDD_TP2/prefix.cpp
@@ -2,35 +2,50 @@
 
 Prefix::Prefix(string str)
 {
-
+	std::vector<char> tableau(str.begin(), str.end());
+	std::stack<char> pile;
 }
 
 Prefix::~Prefix()
 {
 
 }
-bool prefixe(vector<char> tableau)
+bool Prefix::prefixe(vector<char> tableau)
+{
+	bool result = true;
+	char listeCaracteresAutorises[] = "0123456789+-/*()";
+	bool caractereEstValide = true;
+
+	for (vector<char>::iterator i = tableau.begin(); i != tableau.end() && caractereEstValide; i++)
+	{
+		caractereEstValide = false;
+		for (int j = 0; j < sizeof(listeCaracteresAutorises) / sizeof(char) && !caractereEstValide; j++)
+		{
+			if (*i == listeCaracteresAutorises[j]) caractereEstValide = true;
+		}
+	}
+	return caractereEstValide;
+}
+
+bool Prefix::valider_expression(vector<char> tableau)
+{
+	bool result = false;
+	return result;
+}
+
+void Prefix::transformerEnNombres(vector <char> tableau)
 {
 
 }
 
-bool valider_expression(vector<char> tableau)
+void Prefix::transformerEnPrefixe(stack<char> Pile, vector<char> tableau)
 {
 
 }
 
-void transformerEnNombres(vector <char> tableau)
+int Prefix::evaluer_expression(stack<char> Pile, vector<char> tableau)
 {
-
-}
-
-void transformerEnErefixe(stack<char> Pile, vector<char> tableau)
-{
-
-}
-
-int evaluer_expression(stack<char> Pile, vector<char> tableau)
-{
-
+	int result = 0;
+	return result;
 }
 
