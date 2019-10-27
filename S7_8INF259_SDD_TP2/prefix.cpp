d0952d5ce921c2c1fd129e602a6cf999a1828631
smahot <steve.mahot@edu.devinci.fr>
2019-10-27 13:15:47 -0400

    classe prefix

---
+++ S7_8INF259_SDD_TP2/prefix.cpp
@@ -0,0 +1,36 @@
+#include "prefix.h"
+
+Prefix::Prefix(string str)
+{
+
+}
+
+Prefix::~Prefix()
+{
+
+}
+bool prefixe(vector<char> tableau)
+{
+
+}
+
+bool valider_expression(vector<char> tableau)
+{
+
+}
+
+void transformerEnNombres(vector <char> tableau)
+{
+
+}
+
+void transformerEnErefixe(stack<char> Pile, vector<char> tableau)
+{
+
+}
+
+int evaluer_expression(stack<char> Pile, vector<char> tableau)
+{
+
+}
+
---
+++ S7_8INF259_SDD_TP2/prefix.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Prefix
+{
+private:
+	std::stack<char> pile;
+	std::vector<char> Tableau;
+public:
+	Prefix(string str);
+	~Prefix();
+	bool prefixe(vector<char> tableau);// lit l’expression à évaluer, à partir du clavier, dans tableauet valide si l’expression ne contient que les caractères ci - dessus, à savoir les nombres entiers composés de caractères nombres et les opérateurs ci - dessus.
+	bool valider_expression(vector<char> tableau); // (bonus) teste si l’expression lue contient un nombre valide de parenthèses
+	void transformerEnNombres(vector <char> tableau); // transforme les nombres lus en caractères en valeurs numériques
+	void transformerenprefixe(stack<char> Pile, vector<char> tableau);// transforme l’expression lue en une expression préfixée et l’afficher.
+	int evaluer_expression(stack<char> Pile, vector<char> tableau); // évalue l’expression préfixée et affiche sa valeur.
+
+};
